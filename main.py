0a76820703bd1076e5d2b4c833685b00578f4b68
Deepakkoli93 <Deepakkoli93@gmail.com>
2017-04-19 18:39:39 +0530

    Add a logger

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 import base64
 import requests
+import logging
 
+logger = logging.getLogger(__name__)
 BASE_URI = "api.twitter.com"
 CONSUMER_KEY = "NqTvP1Q6xGveBuNLPyjA2uBcU"
 CONSUMER_SECRET = "8BK8mMOFumxWQV2wWdBEQ9ZDWpVpFK7mIIvkTvmQspOo16tkFD"
@@ -11,7 +13,7 @@ class Twitter_api(object):
 	def __init__(self):
 		self.bearer_token = ""
 
-	def fetch_bearer_token(self):
+	def _fetch_bearer_token(self):
 		resource_path = "/oauth2/token"
 		headers = {"Authorization": "Basic " + base64.b64encode(bytes(CONSUMER_KEY+":"+CONSUMER_SECRET))}
 		headers["Content-Type"] = "application/x-www-form-urlencoded"
@@ -20,26 +22,23 @@ class Twitter_api(object):
 		# print(response)
 		# print(response.text)
 		# print(dir(response))
-		# print(response.status_code)
+		logger.debug("Respons status code of the auth request :" + response.status_code)
 		if response.status_code == 200:
-			print(response.json()["access_token"])
+			logger.debug("Bearer access token: " + response.json()["access_token"])
 			self.bearer_token = "Bearer " + response.json()["access_token"]
 		else:
-			print("Not able to get authentication token")
+			logger.error("Not able to get authentication token")
 
 	def fetch_tweets(self):
-		self.fetch_bearer_token()
+		self._fetch_bearer_token()
 		resource_path = "/1.1/search/tweets.json"
 		url = "https://" + BASE_URI + resource_path
 		headers = {"Authorization": self.bearer_token}
 		payload = {"q" : "#custerv"}
 		response = requests.get(url, headers = headers, params=payload)
 		r = response.json()["statuses"]
-		print(r[0])
-		# print(r[10])
-		print(len(response.json()["statuses"]))
-		print(response.json()["search_metadata"])
 
 if __name__ == "__main__":
+	logging.basicConfig()
 	api_client = Twitter_api()
 	api_client.fetch_tweets()
