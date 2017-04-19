630d8f0e46b1ccc097feb945cf2efaf967e893d4
Deepakkoli93 <Deepakkoli93@gmail.com>
2017-04-19 18:27:37 +0530

    Add method to fetch tweets

--- main.py
+++ main.py
@@ -23,9 +23,23 @@ class Twitter_api(object):
 		# print(response.status_code)
 		if response.status_code == 200:
 			print(response.json()["access_token"])
-			self.bearer_token = response.json()["access_token"]
+			self.bearer_token = "Bearer " + response.json()["access_token"]
+		else:
+			print("Not able to get authentication token")
 
+	def fetch_tweets(self):
+		self.fetch_bearer_token()
+		resource_path = "/1.1/search/tweets.json"
+		url = "https://" + BASE_URI + resource_path
+		headers = {"Authorization": self.bearer_token}
+		payload = {"q" : "#custerv"}
+		response = requests.get(url, headers = headers, params=payload)
+		r = response.json()["statuses"]
+		print(r[0])
+		# print(r[10])
+		print(len(response.json()["statuses"]))
+		print(response.json()["search_metadata"])
 
 if __name__ == "__main__":
 	api_client = Twitter_api()
-	api_client.fetch_bearer_token()
+	api_client.fetch_tweets()
