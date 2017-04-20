2a54c5d1c1bd3767b8bf959d7cc69d4868189573
Deepakkoli93 <Deepakkoli93@gmail.com>
2017-04-20 21:22:56 +0530

    Get port number from environ

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
  
@@ -6,4 +7,5 @@ def index():
     return render_template("display_tweets.html")
  
 if __name__ == "__main__":
-    app.run(port=1234)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
